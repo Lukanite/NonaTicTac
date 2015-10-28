9810d92bca7ddd5b20a008fae1abb0e202d9a261
Alan Zhang <lucario@lucar.io>
2015-10-28 15:01:22 -0400

    Drawing X code

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -25,7 +25,7 @@ public class TicTacToe extends JFrame{
 			}
 		}
 		drawTicTac(g,333,333,500/3);
-	
+		drawX(g,0,0,500/9);
 	}
 	public void drawTicTac(Graphics g, int startx, int starty, int size) {
 		//System.out.println(startx + "," + starty + "," + size);
@@ -34,7 +34,17 @@ public class TicTacToe extends JFrame{
 		g.fillRect(startx+size/3, starty, 10*size/500, size);
 		g.fillRect(startx+2*size/3, starty, 10*size/500, size);
 	}
-	
+	public void drawX(Graphics g, int startx, int starty, int size) {
+		int[] xpoints = {23,93,133,178,233,163,243,173,121,70,13,93};
+		int[] ypoints = {0,0,77,0,0,118,256,256,163,256,256,120};
+		for (int i = 0; i<12; i++) {
+			xpoints[i] = xpoints[i]*size/256;
+			ypoints[i] = ypoints[i]*size/256;
+		}
+		g.fillPolygon(xpoints, ypoints, 12); 
+	}
+	public void drawO(Graphics g, int startx, int starty, int size) {
+	}
 	public void drawBoard(Graphics g){
 		
 	}
