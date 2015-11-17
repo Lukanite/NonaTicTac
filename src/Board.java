38348c9138ff6df37987592179a133678c978ac5
anarvekar <an5vq@virginia.edu>
2015-11-17 03:38:56 -0600

    Final - All Components

--- src/Board.java
+++ src/Board.java
@@ -4,92 +4,107 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 
 public class Board {
-	//0 for none, 1 for X, 2 for O
-	private int[] spaces = {0,0,0,0,0,0,0,0,0};
+	// 0 for none, 1 for X, 2 for O
+	private int[] spaces = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 	private int id;
 	private int size;
 	private int xpos;
 	private int ypos;
 	private int winner = 0;
 	private boolean active = false;
-	Color transparent = new Color(255,255,255,255);
+	Color transparent = new Color(255, 255, 255, 255);
+
 	public int getWinner() {
 		return winner;
 	}
-	
+
 	public Board() {
-		//Empty constructor
+		// Empty constructor
 	}
-	
+
 	public Board(int id, int size, int xpos, int ypos) {
-		this.id=id;
+		this.id = id;
 		this.size = size;
 		this.xpos = xpos;
 		this.ypos = ypos;
 	}
+
 	public void draw(Graphics g) {
 		drawTicTac(g, this.xpos, this.ypos, this.size);
 		for (int y = 0; y < 3; y++) {
 			for (int x = 0; x < 3; x++) {
-				//take into account sizes of borders
-				if (spaces[y*3+x]==1) drawX(g, xpos+(x*size/3)+10*size/500, ypos+(y*size/3)+10*size/500, (int)(size/3.15));
-				else if (spaces[y*3+x]==2) drawO(g, xpos+(x*size/3)+10*size/500, ypos+(y*size/3)+10*size/500, (int)(size/3.15));
+				// take into account sizes of borders
+				if (spaces[y * 3 + x] == 1)
+					drawX(g, xpos + (x * size / 3) + 10 * size / 500, ypos + (y * size / 3) + 10 * size / 500,
+							(int) (size / 3.15));
+				else if (spaces[y * 3 + x] == 2)
+					drawO(g, xpos + (x * size / 3) + 10 * size / 500, ypos + (y * size / 3) + 10 * size / 500,
+							(int) (size / 3.15));
 			}
 		}
 	}
+
 	public void drawTicTac(Graphics g, int startx, int starty, int size) {
 		g.setColor(Color.BLACK);
-		if (winner>0 || isFull()) g.setColor(new Color(128,128,128));
-		g.fillRect(startx, starty+size/3, size, 10*size/500);
-		g.fillRect(startx, starty+2*size/3, size, 10*size/500);
-		g.fillRect(startx+size/3, starty, 10*size/500, size);
-		g.fillRect(startx+2*size/3, starty, 10*size/500, size);
+		if (winner > 0 || isFull())
+			g.setColor(new Color(128, 128, 128));
+		g.fillRect(startx, starty + size / 3, size, 10 * size / 500);
+		g.fillRect(startx, starty + 2 * size / 3, size, 10 * size / 500);
+		g.fillRect(startx + size / 3, starty, 10 * size / 500, size);
+		g.fillRect(startx + 2 * size / 3, starty, 10 * size / 500, size);
 	}
+
 	public void drawX(Graphics g, int startx, int starty, int size) {
 		g.setColor(Color.RED);
-		if (winner>0 || isFull()) g.setColor(new Color(255,128,128));
-		int[] xpoints = {23,93,133,178,233,163,243,173,121,70,13,93};
-		int[] ypoints = {0,0,77,0,0,118,256,256,163,256,256,120};
-		for (int i = 0; i<12; i++) {
-			xpoints[i] = (xpoints[i]*size/256)+startx;
-			ypoints[i] = (ypoints[i]*size/256)+starty;
+		if (winner > 0 || isFull())
+			g.setColor(new Color(255, 128, 128));
+		int[] xpoints = { 23, 93, 133, 178, 233, 163, 243, 173, 121, 70, 13, 93 };
+		int[] ypoints = { 0, 0, 77, 0, 0, 118, 256, 256, 163, 256, 256, 120 };
+		for (int i = 0; i < 12; i++) {
+			xpoints[i] = (xpoints[i] * size / 256) + startx;
+			ypoints[i] = (ypoints[i] * size / 256) + starty;
 		}
-		g.fillPolygon(xpoints, ypoints, 12); 
+		g.fillPolygon(xpoints, ypoints, 12);
 	}
+
 	public void drawO(Graphics g, int startx, int starty, int size) {
 		g.setColor(Color.BLUE);
-		if (winner>0 || isFull()) g.setColor(new Color(128,128,255));
-		//g.fillOval(startx, starty, size, size);
-		//g.setColor(transparent);
-		//g.fillOval(startx+size/5, starty+size/5, (int)Math.round(.6*size), (int)Math.round(.6*size));
+		if (winner > 0 || isFull())
+			g.setColor(new Color(128, 128, 255));
+		// g.fillOval(startx, starty, size, size);
+		// g.setColor(transparent);
+		// g.fillOval(startx+size/5, starty+size/5, (int)Math.round(.6*size),
+		// (int)Math.round(.6*size));
 		Graphics2D g2 = (Graphics2D) g;
-		g2.setStroke(new BasicStroke((int)(size/5.5)));
-		g2.drawOval(startx+size/10, starty+size/10, (int)(.8*size), (int)(.8*size));
+		g2.setStroke(new BasicStroke((int) (size / 5.5)));
+		g2.drawOval(startx + size / 10, starty + size / 10, (int) (.8 * size), (int) (.8 * size));
 	}
+
 	public int getXpos() {
 		return xpos;
 	}
+
 	public int getYpos() {
 		return ypos;
 	}
+
 	public int[] getSpaces() {
 		return spaces;
 	}
-	
+
 	public boolean processClick(int x, int y) {
 		// TODO Auto-generated method stub
-		
+
 		boolean result = false;
-		int xpart = x/(size/3);
-		int ypart = y/(size/3);
-		int space = 3*ypart+xpart;
+		int xpart = x / (size / 3);
+		int ypart = y / (size / 3);
+		int space = 3 * ypart + xpart;
 		System.out.println(space);
 		if (spaces[space] == 0 && isActive()) {
 			if (TicTacToe.isXturn()) {
 				spaces[space] = 1;
 				TicTacToe.setXturn(false);
-			}
-			else {
+			} else {
 				spaces[space] = 2;
 				TicTacToe.setXturn(true);
 			}
@@ -98,69 +113,79 @@ public class Board {
 		checkWin();
 		return result;
 	}
-	
+
+	// AI process click
 	public boolean processClick(int space) {
 		// TODO Auto-generated method stub
-		System.out.println(space);
 		
+		System.out.println(space);
+
 		boolean result = false;
 		if (spaces[space] == 0 && isActive()) {
 			if (TicTacToe.isXturn()) {
 				spaces[space] = 1;
+				//make the computer play O
 				TicTacToe.setXturn(false);
-			}
-			else {
+			} else {
 				spaces[space] = 2;
 				TicTacToe.setXturn(true);
-			}
+			} 
 			result = true;
 		}
 		checkWin();
 		return result;
 	}
-	
+
 	public void checkWin() {
 		int win = 0;
-		for (int p = 1; p<3; p++) {
-			//check horizontal
-			for (int y = 0; y<3; y++) {
-				if (spaces[y*3]==p && spaces[y*3+1]==p && spaces[y*3+2]==p) win=p;
+		for (int p = 1; p < 3; p++) {
+			// check horizontal
+			for (int y = 0; y < 3; y++) {
+				if (spaces[y * 3] == p && spaces[y * 3 + 1] == p && spaces[y * 3 + 2] == p)
+					win = p;
 			}
-			//check vertical
-			for (int x = 0; x<3; x++) {
-				if (spaces[x]==p && spaces[x+3]==p && spaces[x+6]==p) win = p;
+			// check vertical
+			for (int x = 0; x < 3; x++) {
+				if (spaces[x] == p && spaces[x + 3] == p && spaces[x + 6] == p)
+					win = p;
 			}
-			//check diagonal
-			if (spaces[2]==p && spaces[4]==p && spaces[6]==p) win = p;
-			if (spaces[0]==p && spaces[4]==p && spaces[8]==p) win = p;
+			// check diagonal
+			if (spaces[2] == p && spaces[4] == p && spaces[6] == p)
+				win = p;
+			if (spaces[0] == p && spaces[4] == p && spaces[8] == p)
+				win = p;
 		}
 		this.winner = win;
-		if (winner>0) {
+		if (winner > 0) {
 			active = false;
 			if (this.id != 9) {
 				TicTacToe.boards[9].spaces[this.id] = winner;
 				TicTacToe.boards[9].checkWin();
-			}
-			else {
+			} else {
 				System.out.println("Player " + winner + " wins!");
 				TicTacToe.setWin(winner);
 			}
 		} else {
-			//check if the board is full
-			
-			if (isFull()) active = false;
+			// check if the board is full
+
+			if (isFull())
+				active = false;
 		}
 	}
+
 	public boolean isFull() {
 		boolean full = true;
-		for (int space:spaces) if (space==0) full=false;
+		for (int space : spaces)
+			if (space == 0)
+				full = false;
 		return full;
 	}
+
 	public boolean isActive() {
 		return active;
 	}
+
 	public void setActive(boolean active) {
 		this.active = active;
 	}
 }
-
--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -11,12 +12,14 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class TicTacToe extends JPanel{
+public class TicTacToe extends JPanel {
 	private static final long serialVersionUID = 9106403499068281271L;
 	static Board boards[] = new Board[10];
+	static Color background = new Color(255, 255, 200);
+	static Color transparent = new Color(255, 255, 255, 255);
 	static boolean xturn = true;
-	static Color background = new Color(255,255,200);
-	static Color transparent = new Color(255,255,255,255);
+	boolean playC = false;
+
 	public static boolean isXturn() {
 		return xturn;
 	}
@@ -24,8 +27,9 @@ public class TicTacToe extends JPanel{
 	public static void setXturn(boolean xturn) {
 		TicTacToe.xturn = xturn;
 	}
+
 	static int win = 0;
-	
+
 	public static int getWin() {
 		return win;
 	}
@@ -34,8 +38,11 @@ public class TicTacToe extends JPanel{
 		TicTacToe.win = win;
 	}
 
-	public TicTacToe(){
+	public TicTacToe() {
 		setLayout(null);
+		JButton playComp = new JButton("Play the Computer");
+		playComp.setBounds(200, 500, 189, 23);
+		add(playComp);
 		JButton resetButton = new JButton("Reset");
 		resetButton.setBounds(0, 500, 89, 23);
 		add(resetButton);
@@ -46,16 +53,17 @@ public class TicTacToe extends JPanel{
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				// TODO Auto-generated method stub
-				for (int y = 0; y<3; y++) {
-					for (int x = 0; x<3; x++) {
-						boards[y*3+x] = new Board(y*3+x, 500/3, x*500/3, y*500/3);
+				for (int y = 0; y < 3; y++) {
+					for (int x = 0; x < 3; x++) {
+						boards[y * 3 + x] = new Board(y * 3 + x, 500 / 3, x * 500 / 3, y * 500 / 3);
 					}
 				}
 				boards[9] = new Board(9, 500, 0, 0);
 				win = 0;
 				repaint();
+				xturn = true;
 			}
-			
+
 		};
 		ActionListener exitlistener = new ActionListener() {
 			@Override
@@ -63,6 +71,24 @@ public class TicTacToe extends JPanel{
 				System.exit(0);
 			}
 		};
+
+		ActionListener playCompListener = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+
+				playC = true;
+				for (int y = 0; y < 3; y++) {
+					for (int x = 0; x < 3; x++) {
+						boards[y * 3 + x] = new Board(y * 3 + x, 500 / 3, x * 500 / 3, y * 500 / 3);
+					}
+				}
+				boards[9] = new Board(9, 500, 0, 0);
+				win = 0;
+				repaint();
+				xturn = true;
+			}
+		};
+
 		MouseListener listener = new MouseListener() {
 
 			@Override
@@ -73,120 +99,147 @@ public class TicTacToe extends JPanel{
 			@Override
 			public void mouseEntered(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
 
 			@Override
 			public void mouseExited(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
 
 			@Override
 			public void mousePressed(MouseEvent arg0) {
-				boolean outcome = TicTacToe.processClick(arg0.getX(),arg0.getY());
+				boolean outcome = TicTacToe.processClick(arg0.getX(), arg0.getY());
 				repaint();
-				if(outcome) {
-					AI();
-					repaint();
-				}				
+				/*
+				 * if (outcome && playC) { AI(); repaint(); }
+				 */
+				if (playC) {
+					Board activeBoard = null;
+					for (int i = 0; i < 9; i++) {
+
+						if (boards[i].isActive()) {
+							activeBoard = boards[i];
+						}
+					}
+					if (activeBoard == null && isXturn() == false) {
+						ArrayList<Board> empBoards = new ArrayList<Board>();
+						for (int i = 0; i < 9; i++) {
+							if (boards[i].getWinner() == 0)
+								empBoards.add(boards[i]);
+						}
+						Random generator = new Random();
+						int index = generator.nextInt(empBoards.size());
+						Board move = empBoards.get(index);
+						move.setActive(true);
+						AI();
+					}
+					else{
+						AI();
+						repaint();
+					}
+				}
 			}
 
 			@Override
 			public void mouseReleased(MouseEvent arg0) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 		};
 		addMouseListener(listener);
 		resetButton.addActionListener(resetListener);
 		exitButton.addActionListener(exitlistener);
+		playComp.addActionListener(playCompListener);
 		setFocusable(true);
 	}
-	
-	
+
 	protected static boolean processClick(int x, int y) {
-		int boardxpos = x/(500/3);
-		int boardypos = y/(500/3);
-		//check if no boards are active
+		int boardxpos = x / (500 / 3);
+		int boardypos = y / (500 / 3);
+		// check if no boards are active
 		boolean boardactive = false;
-		for (Board b:boards) if (b.isActive()) boardactive = true;
-		if (!boardactive && boards[3*boardypos+boardxpos].getWinner()==0) boards[3*boardypos+boardxpos].setActive(true);
-		return boards[3*boardypos+boardxpos].processClick(x-boards[3*boardypos+boardxpos].getXpos(), y-boards[3*boardypos+boardxpos].getYpos());
+		for (Board b : boards)
+			if (b.isActive())
+				boardactive = true;
+		if (!boardactive && boards[3 * boardypos + boardxpos].getWinner() == 0)
+			boards[3 * boardypos + boardxpos].setActive(true);
+		return boards[3 * boardypos + boardxpos].processClick(x - boards[3 * boardypos + boardxpos].getXpos(),
+				y - boards[3 * boardypos + boardxpos].getYpos());
 	}
 
-	
-	public void paint(Graphics g){
+	public void paint(Graphics g) {
 		super.paint(g);
 		boolean draw = true;
-		
+
 		if (win == 0) {
-			for(int i = 0; i < boards.length-1; i++) {
+			for (int i = 0; i < boards.length - 1; i++) {
 				if (!boards[i].isFull()) {
 					draw = false;
 				}
 				boards[i].draw(g);
 			}
 			boards[9].draw(g);
-			
-			if(draw) {
-				g.drawString("It's a draw!", 100, 100);
+
+			if (draw) {
+				g.setFont(new Font("TimesRoman", Font.BOLD, 50));
+				g.drawString("It's a draw!", 120, 250);
 			}
-		}
-		else {
-			g.drawString("Player " + win + " wins!", 100, 100);
+		} else {
+			g.setFont(new Font("TimesRoman", Font.BOLD, 50));
+			g.drawString("Player " + win + " wins!", 120, 250);
 		}
 	}
-	
+
 	public void AI() {
 		Board activeBoard = new Board();
-		for(Board b : boards) {
+		for (Board b : boards) {
 			if (b.isActive()) {
 				activeBoard = b;
 			}
 		}
 		System.out.println(activeBoard);
-		
-		ArrayList<Integer> emptySpaces = new ArrayList<Integer>(); 
-		
-		for(int i = 0; i < activeBoard.getSpaces().length; i++) {
+
+		ArrayList<Integer> emptySpaces = new ArrayList<Integer>();
+
+		for (int i = 0; i < activeBoard.getSpaces().length; i++) {
 			if (activeBoard.getSpaces()[i] == 0) {
 				emptySpaces.add(i);
 				System.out.print(i);
 			}
 		}
 		System.out.println();
-		
+
 		Random generator = new Random();
 		int index = generator.nextInt(emptySpaces.size());
 		int move = emptySpaces.get(index);
 		System.out.print("AI: ");
 		System.out.println(move);
 		activeBoard.processClick(move);
-		
+
 	}
-	
-	public static void main(String[] args){
-		
+
+	public static void main(String[] args) {
+
 		JFrame frame = new JFrame("Tic Tac Nine");
-		TicTacToe tictac  = new TicTacToe();
+		TicTacToe tictac = new TicTacToe();
 		tictac.setBackground(background);
 		frame.add(tictac);
 		frame.setSize(520, 580);
 		frame.setLocationRelativeTo(null);
-		
+
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		for (int y = 0; y<3; y++) {
-			for (int x = 0; x<3; x++) {
-				boards[y*3+x] = new Board(y*3+x, 500/3, x*500/3, y*500/3);
+		for (int y = 0; y < 3; y++) {
+			for (int x = 0; x < 3; x++) {
+				boards[y * 3 + x] = new Board(y * 3 + x, 500 / 3, x * 500 / 3, y * 500 / 3);
 			}
 		}
 		boards[9] = new Board(9, 500, 0, 0);
 		tictac.repaint();
+		xturn = true;
 	}
-	
-	
-}
 
+}
