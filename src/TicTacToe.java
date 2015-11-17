293cb7ad9c330f4379942c0423dbfc2d3a48d0ec
Neel Patel <np2ch@virginia.edu>
2015-11-17 00:32:34 -0500

    commented

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,11 +1,18 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import javax.swing.*;
 
-public class TicTacToe extends JFrame{
+public class TicTacToe extends JPanel {
 	private static final long serialVersionUID = 9106403499068281271L;
+	static Board boards[] = new Board[10];
 	static boolean xturn = true;
-	static Color background = new Color(255,255,200);
-	static Color transparent = new Color(255,255,255,255);
+	static Color background = new Color(255, 255, 200);
+	static Color transparent = new Color(255, 255, 255, 255);
+
 	public static boolean isXturn() {
 		return xturn;
 	}
@@ -13,8 +20,9 @@ public class TicTacToe extends JFrame{
 	public static void setXturn(boolean xturn) {
 		TicTacToe.xturn = xturn;
 	}
+
 	static int win = 0;
-	
+
 	public static int getWin() {
 		return win;
 	}
@@ -22,68 +30,140 @@ public class TicTacToe extends JFrame{
 	public static void setWin(int win) {
 		TicTacToe.win = win;
 	}
-	public TicTacToe(){
-		init();
-	}
-	
-	private void init(){
-		setSize(500, 500);
-		setLocationRelativeTo(null);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-//		THIS IS A GITHUB TEST BY ADITI
-	}
-	
-	public void paint(Graphics g){
-		//g.fillRect(0, 167, 500, 10);
-		//g.fillRect(0, 333, 500, 10);
-		//g.fillRect(333, 0, 10, 500);
-		//g.fillRect(167, 0, 10, 500);
-		drawTicTac(g, 0, 0, 500);
-		for (int x = 0; x<3; x++) {
-			for (int y = 0; y<3; y++) {
-				drawTicTac(g,x*500/3, y*500/3, 500/3);
-			}
-		}
-		drawTicTac(g,333,333,500/3);
-		drawX(g,0,0,500/9);
-		drawO(g,0,500/9,500/9);
-	}
-	public void drawTicTac(Graphics g, int startx, int starty, int size) {
-		//System.out.println(startx + "," + starty + "," + size);
-		g.fillRect(startx, starty+size/3, size, 10*size/500);
-		g.fillRect(startx, starty+2*size/3, size, 10*size/500);
-		g.fillRect(startx+size/3, starty, 10*size/500, size);
-		g.fillRect(startx+2*size/3, starty, 10*size/500, size);
-	}
-	public void drawX(Graphics g, int startx, int starty, int size) {
-		int[] xpoints = {23,93,133,178,233,163,243,173,121,70,13,93};
-		int[] ypoints = {0,0,77,0,0,118,256,256,163,256,256,120};
-		for (int i = 0; i<12; i++) {
-			xpoints[i] = xpoints[i]*size/256;
-			ypoints[i] = ypoints[i]*size/256;
-		}
-		g.fillPolygon(xpoints, ypoints, 12); 
-	}
-	public void drawO(Graphics g, int startx, int starty, int size) {
-		g.fillOval(startx, starty, size, size);
-		g.setColor(Color.LIGHT_GRAY); //Currently hardcoded, should change
-		g.fillOval(startx+size/5, starty+size/5, (int)Math.round(.6*size), (int)Math.round(.6*size));
-	}
-	public void drawBoard(Graphics g){
-		
-	}
-	
-	public static void main(String[] args){
-		
-		EventQueue.invokeLater(new Runnable() {
-			
+
+	public TicTacToe() {
+		setLayout(null);
+		JButton playComp = new JButton("Play the Computer");
+		playComp.setBounds(200, 500, 189, 23);
+		add(playComp);
+		JButton resetButton = new JButton("Reset");
+		resetButton.setBounds(0, 500, 89, 23);
+		add(resetButton);
+		JButton exitButton = new JButton("Exit");
+		exitButton.setBounds(100, 500, 89, 23);
+		//fdgdf'lgksd
+		add(exitButton);
+		ActionListener resetListener = new ActionListener() {
 			@Override
-			public void run() {
-				TicTacToe x  = new TicTacToe();
-				x.setVisible(true);
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				for (int y = 0; y < 3; y++) {
+					for (int x = 0; x < 3; x++) {
+						boards[y * 3 + x] = new Board(y * 3 + x, 500 / 3, x * 500 / 3, y * 500 / 3);
+					}
+				}
+				boards[9] = new Board(9, 500, 0, 0);
+				win = 0;
+				repaint();
 			}
-		});
+
+		};
+		ActionListener exitlistener = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0);
+			}
+		};
+
+		ActionListener playCompListener = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("hello");
+			}
+		};
+		MouseListener listener = new MouseListener() {
+
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				TicTacToe.processClick(arg0.getX(), arg0.getY());
+				repaint();
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+		};
+		addMouseListener(listener);
+		resetButton.addActionListener(resetListener);
+		exitButton.addActionListener(exitlistener);
+		playComp.addActionListener(playCompListener);
+		setFocusable(true);
 	}
-	
+
+	protected static void processClick(int x, int y) {
+		int boardxpos = x / (500 / 3);
+		int boardypos = y / (500 / 3);
+		// check if no boards are active
+		boolean boardactive = false;
+		for (Board b : boards)
+			if (b.isActive())
+				boardactive = true;
+		if (!boardactive && boards[3 * boardypos + boardxpos].getWinner() == 0)
+			boards[3 * boardypos + boardxpos].setActive(true);
+		boards[3 * boardypos + boardxpos].processClick(x - boards[3 * boardypos + boardxpos].getXpos(),
+				y - boards[3 * boardypos + boardxpos].getYpos());
+	}
+
+	protected static void randX() {
+		int pos = (int) (Math.random() * 9);
+
+	}
+
+	public void paint(Graphics g) {
+		super.paint(g);
+		if (win == 0) {
+			if (boards[9].isFull()) {
+				g.setFont(new Font("TimesRoman", Font.BOLD, 50));
+				g.drawString("It's a draw!", 110, 250);
+			} else
+				for (Board b : boards)
+					b.draw(g);
+		} else {
+			g.setFont(new Font("TimesRoman", Font.BOLD, 50));
+			g.drawString("Player " + win + " wins!", 110, 250);
+		}
+	}
+
+	public static void main(String[] args) {
+
+		JFrame frame = new JFrame("Tic Tac Nine");
+		TicTacToe tictac = new TicTacToe();
+		tictac.setBackground(background);
+		frame.add(tictac);
+		frame.setSize(520, 580);
+		frame.setLocationRelativeTo(null);
+
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		for (int y = 0; y < 3; y++) {
+			for (int x = 0; x < 3; x++) {
+				boards[y * 3 + x] = new Board(y * 3 + x, 500 / 3, x * 500 / 3, y * 500 / 3);
+			}
+		}
+		boards[9] = new Board(9, 500, 0, 0);
+		tictac.repaint(); 
+	}
 	
 }
